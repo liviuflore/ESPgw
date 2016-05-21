f060653795a3e3d1756dc51e7886076f31046b77
liviuflore <liviuflore@gmail.com>
2016-05-21 16:05:02 +0300

    add wifi setup

--- ESPgw.ino
+++ ESPgw.ino
@@ -9,7 +9,8 @@
 #define ErrorWifi 1
 #define ErrorUpgrade 2
 int ErrorList[] = {0, 0, 0};
-#define ERR(val, msg)        { ErrorList[val] = 1; Serial.printf(msg); }
+#define ERR(val, ...)       { ErrorList[val] = 1; Serial.printf(__VA_ARGS__); }
+#define LOG(...)			Serial.printf(__VA_ARGS__)
 
 #define ESPconfig_version 1
 const char* ESPconfig_APssid = "ESPgw";
@@ -24,30 +25,59 @@ struct ESPconfig {
 
 EEPROMconfig<ESPconfig> cfg(ESPconfig_version);
 
-void setup() {
+void setup_wifi()
+{
+	LOG("ssid:   %s\n", cfg.data.ssid);
+	LOG("pass:   %s\n", cfg.data.password);
+	if (cfg.data.ssid[0] == 0 || cfg.data.password[0] == 0) {
+		LOG("No wifi configuration found, starting in AP mode\n");
+		WiFi.mode(WIFI_AP);
+		WiFi.softAP(ESPconfig_APssid, ESPconfig_APpassword);
+		LOG("Please connect to:\n");
+		LOG("ssid:   %s\n", ESPconfig_APssid);
+		LOG("pass:   %s\n", ESPconfig_APpassword);
+		LOG("ip:     "); Serial.println(WiFi.softAPIP());
+	}
+	else {
+		int retry_count = 30;
+		LOG("Connecting to: %s\n", cfg.data.ssid);
+		WiFi.mode(WIFI_STA);
+		WiFi.begin(cfg.data.ssid, cfg.data.password);
+		while (WiFi.status() != WL_CONNECTED && retry_count-- > 0) {
+			delay(1000);
+			LOG(".");
+		}
+		if (WiFi.status() != WL_CONNECTED) { 
+			WiFi.disconnect();
+			delay(1000);
+			LOG("Couldn't connect to network, starting in AP mode\n");
+			WiFi.mode(WIFI_AP);
+			WiFi.softAP(ESPconfig_APssid, ESPconfig_APpassword);
+			LOG("Please connect to:\n");
+			LOG("ssid:   %s\n", ESPconfig_APssid);
+			LOG("pass:   %s\n", ESPconfig_APpassword);
+			LOG("ip:     "); Serial.println(WiFi.softAPIP());
+		}
+		else {
+			LOG("Connected to %s\n", cfg.data.ssid);
+			LOG("local ip: "); Serial.println(WiFi.localIP());
+		}
+
+	}
+}
+
+void setup()
+{
     Serial.begin(74800);
     delay(500);
     
     if (!cfg.load()) {
         ERR(ErrorEeprom, "EEPROM config load failed, will load defaults\n");
-		cfg.save();
-//		while (true);
+//		cfg.save();
     }
 	Serial.printf("EEPROM config load done\n");
-
-    Serial.printf("loaded: %d\n", cfg.loaded);
-    Serial.printf("ssid:   %s\n", cfg.data.ssid);
-	Serial.printf("pass:   %s\n", cfg.data.password);
-
-	if (cfg.data.ssid[0] == 0 || cfg.data.password[0] == 0) {
-		Serial.printf("No wifi configuration found, starting in AP mode\n");
-		WiFi.mode(WIFI_AP);
-		WiFi.softAP(ESPconfig_APssid, ESPconfig_APpassword);
-		Serial.printf("Please connect to:\n");
-		Serial.printf("ssid:   %s\n", ESPconfig_APssid);
-		Serial.printf("pass:   %s\n", ESPconfig_APpassword);
-		Serial.printf("ip:     "); Serial.println(WiFi.softAPIP());
-	}
+	
+	setup_wifi();
 }
 
 void loop() {
